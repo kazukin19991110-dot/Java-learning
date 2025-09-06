f9da72eda4ebc1cbf8d953dbafaa500fc5a9afb1
Matcha01 <kazukin1999.1110@gmail.com>
2025-09-06 18:45:23 +0900

    add Method_void.java

--- Paiza-basic/Class/Method_void.java
+++ Paiza-basic/Class/Method_void.java
@@ -2,19 +2,23 @@ public class Method_void {
     public static void main(String... args) {
         MyfirstClass o1 = new MyfirstClass();
         o1.name = "Kyoko";
+        MyfirstClass o2 = new MyfirstClass();
+        o2.name = "Rio";
 
-        o1.printName();
+        System.out.println(o1.getName() + "（戻り値ありのメソッド）");
+        o2.printName();
     }
 }
 
 class MyfirstClass {
     String name;
 
+    //戻り値ありのメソッド
     String getName() {
         return "私の名前は" + name + "です。";
     }
-
+    //戻り値なしのメソッド
     void printName() {
-        System.out.println("私の名前は" + name + "です。");
+        System.out.println("私の名前は" + name + "です。" + "（戻り値なしのメソッド）");
     }
 }
