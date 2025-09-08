70c81bcc7d57645193770623711cf0de3f2ea673
Matcha01 <kazukin1999.1110@gmail.com>
2025-09-08 11:36:18 +0900

    javafile

--- Paiza-basic/Practice/Class.java
+++ Paiza-basic/Practice/Class.java
@@ -1,5 +1,24 @@
 public class Class {
     public static void main(String[] args) {
+        Prac p1 = new Prac();
+        Prac p2 = new Prac();
         
-        }
+        p1.message = "こんにちは(戻り値なし)";
+        p1.printMessage(); 
+
+        p2.message = "こんばんわ(戻り値あり)";
+        String message1 = p2.getMessage();
+        System.out.println(message1);
+    }
+}
+
+class Prac {
+    String message;
+
+    String getMessage() {
+        return message;
+    }
+    void printMessage() {
+        System.out.println(message);
+    }
 }
