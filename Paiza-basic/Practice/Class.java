43956e0660ba1249881ea4197b581d8ede35c791
Matcha01 <kazukin1999.1110@gmail.com>
2025-09-08 18:31:49 +0900

    add javafile

--- Paiza-basic/Practice/Class.java
+++ Paiza-basic/Practice/Class.java
@@ -1,14 +1,28 @@
 public class Class {
     public static void main(String[] args) {
         Prac p1 = new Prac();
-        Prac p2 = new Prac();
+        Prac p2 = new Prac("#10：仮引数のあるコンストラクタ");
         
         p1.message = "こんにちは(戻り値なし)";
         p1.printMessage(); 
 
-        p2.message = "こんばんわ(戻り値あり)";
-        String message1 = p2.getMessage();
+        p1.message = "こんばんわ(戻り値あり)";
+        String message1 = p1.getMessage();
         System.out.println(message1);
+
+        //実引数
+        p1.printStringValue("おやすみなさい(仮引数あり)");
+        p1.printAddInt(5, 10);
+
+        //オーバーロード呼び出し
+        p1.printMessage01();
+
+        //static変数呼び出し
+        Prac.number = 700;
+        System.out.println("#11：" + Prac.number);
+
+        //staticメソッド呼び出し
+        Prac.print(813);
     }
 }
 
@@ -20,5 +34,47 @@ class Prac {
     }
     void printMessage() {
         System.out.println(message);
+    }
+
+    //仮引数
+    void printStringValue(String s) {
+        System.out.println(s);
+    }
+    void printAddInt(int i, int j) {
+        System.out.println("引数2つ：" + (i + j));
+    }
+
+    //オーバーロード
+    void printMessage01(String s) {
+        System.out.println(s);
+    }
+    void printMessage01() {
+        printMessage01("おはようございます(オーバーロード)");
+        }
+
+    //デフォルトコンストラクタ
+    public Prac() {
+        System.out.println("#09：デフォルトコンストラクタ");
+    }
+
+    //仮引数ありのコンストラクタ
+    public Prac(String s) {
+        message = s;
+        System.out.println(message);
+    }
+
+    //static変数
+    static int number;
+
+    //staticメソッド
+    String name;
+    static void print(int i) {
+        System.out.println("#12：");
+        System.out.println("paiza");
+        print(String.valueOf(i)); //文字列へ変換「813 ➡︎ "813"」
+    }
+    static void print(String s) {
+        System.out.println("kirishima");
+        System.out.println(s);
     }
 }
--- Paiza-basic/Practice/README.md
+++ Paiza-basic/Practice/README.md
@@ -20,3 +20,21 @@
 ③
 - Pracクラスに、戻り値はなく、インスタンス変数 message を出力する printMessage メソッドを定義
 - printMessageメソッドを Main メソッド内で呼び出す
+
+④
+- Prac クラスに、String 型の値を受け取り、それを出力する printStringValue メソッドを定義
+- printStringValue メソッドを Main メソッド内で呼び出す
+
+⑤
+- Prac クラスに、int 型の値を 2 つ受け取り、それを足し算した値を出力する printAddInt メソッドを定義
+- Main メソッド内で呼び出す
+- printAddInt メソッドの実引数は 5, 10 とする
+
+⑥
+- オーバーロードを使って、Prac クラスに引数を受け取らない printMessage メソッドを定義➡︎mainメソッド内で呼びだす
+- 新しく定義した printMessage メソッドでは、String 型の値を受け取る printMessage メソッドを呼び出す
+- 引数に こんにちは を渡す
+
+⑦
+- デフォルトコンストラクタを定義して、インスタンス化の際に デフォルトコンストラクタ と出力
+
